ec90733b88bfacbd4ab433fc52874847566d87a4
Olesya Miller <olesyam.miller@gmail.com>
2019-12-15 23:44:46 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,34 +1,12 @@
 class Book
+  attr_accessible :author, :page_count, :genre 
+  
   def initialize(title)
     @title = title 
   end
   
   def title
     @title 
-  end
-  
-  def author=(author)
-    @author = author
-  end
-  
-  def author
-    @author
-  end
-  
-  def page_count=(num)
-    @paige_count = num 
-  end
-  
-  def page_count
-    @paige_count
-  end
-  
-  def genre=(genre)
-    @genre = genre
-  end
-  
-  def genre
-    @genre
   end
   
   def turn_page
